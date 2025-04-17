b1e5d1d53a069a47dbe706264ef354c45d479f59
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-17 09:05:31 -0300

    feat: add support for spread operator via Babel plugin

---
+++ webpack/ex.2/index.js
@@ -0,0 +1,4 @@
+import Pessoa from "./pessoa"
+
+const pessoa = new Pessoa('KKau')
+console.log(pessoa.toString())
--- webpack/ex/index.js
+++ webpack/ex/index.js
@@ -1,4 +1,15 @@
-import Pessoa from "./pessoa"
-
-const pessoa = new Pessoa('KKau')
-console.log(pessoa.toString())
+const produto = {
+    nome: 'Caneta Bic Preta',
+    preco: 1.90,
+    desconto: 0.05
+  }
+  
+  function clone(objeto) {
+    return { ...objeto }
+  }
+  
+  const novoProduto = clone(produto)
+  novoProduto.nome = 'Caneta Bic Azul'
+  
+  console.log(produto, novoProduto)
+  
