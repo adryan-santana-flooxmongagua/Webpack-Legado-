c14de78e359ed20e678e6fb59e987c02ab951aff
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-17 09:11:06 -0300

    chore: add Babel preset for React support makefile Copiar Editar

--- webpack/ex/index.js
+++ webpack/ex/index.js
@@ -1,15 +1,7 @@
-const produto = {
-    nome: 'Caneta Bic Preta',
-    preco: 1.90,
-    desconto: 0.05
-  }
-  
-  function clone(objeto) {
-    return { ...objeto }
-  }
-  
-  const novoProduto = clone(produto)
-  novoProduto.nome = 'Caneta Bic Azul'
-  
-  console.log(produto, novoProduto)
-  
+import 'react'
+
+export default props => (
+   <h1> a </h1>
+)
+
+console.log('Funcionou')
